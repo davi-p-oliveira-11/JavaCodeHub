2eabee59d1621e49c721c26ee45e41d3499dc3da
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-02-03 18:47:49 -0300

    Update and rename solution.java to HelloUser.java

--- Challenges/HelloUser/HelloUser.java
+++ Challenges/HelloUser/HelloUser.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class WelcomeUser {
+public class HelloUser {
   public static void main(String[] args) {
     
     Scanner scanner = new Scanner(System.in);
@@ -10,6 +10,9 @@ public class WelcomeUser {
     String userName = scanner.nextLine();
 
     System.out.println("Hello, " + userName + "! Nice to meet you !");
+
+    System.out.println("Press Enter to exit...");
+    scanner.nextLine();
 
     scanner.close();
   }
